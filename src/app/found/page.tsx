a6b1aa0cad49f083487ec221bbf08139c1107149
unknown <arcsmo19@gmail.com>
2024-10-31 11:20:43 +0530

    fix: removed redundancies

--- src/app/found/page.tsx
+++ src/app/found/page.tsx
@@ -1,8 +1,4 @@
-import Main from "@/components/main";
-import Profile from "@/components/profile";
-import Lost from "@/components/lost";
 import Found from "@/components/found";
-import Landing from "@/components/landing";
 
 export default function Home() {
   return (
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,3 @@
-import Main from "@/components/main";
-import Profile from "@/components/profile";
-import Lost from "@/components/lost";
-import Found from "@/components/found";
 import Landing from "@/components/landing";
 
 export default function Home() {
